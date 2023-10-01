4a959930520b7b423947c308f9d852f348bd1be6
kenjitheman <kenjitheman@proton.me>
2023-10-02 01:52:26 +0300

    feat(pages/index.js): chart fetching

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import {
   Box,
   //Button,
-  chakra,
+  //chakra,
   //Container,
   Heading,
   //Link,
@@ -9,73 +9,27 @@ import {
   //ListItem,
   //useColorModeValue,
 } from "@chakra-ui/react";
-import Paragraph from "../components/paragraph";
 import Layout from "../components/layouts/article";
 import Section from "../components/section";
-//import { IoLogoGithub } from "react-icons/io5";
-//import { FaTelegram } from "react-icons/fa";
-import Image from "next/image";
-
-const ProfileImage = chakra(Image, {
-  shouldForwardProp: (prop) => ["width", "height", "src", "alt"].includes(prop),
-});
+import Chart from "../components/chart.js";
 
 const Home = () => (
-  <Layout>
-
-    <Box display={{ md: "flex" }}>
-      <Box flexGrow={1} mt={5}>
+  <Layout textAlign={"center"}>
+    <Box display={{ md: "unset" }}>
+      <Box flexGrow={1} mt={5} textAlign={"center"}>
         <Heading as="h2" variant="page-title">
-          first title here
+          magneconn
         </Heading>
         <p>
-          some title
+          open source | kenjitheman
         </p>
+
+        <Section delay={0.1} mb={5} >
+          <Chart />
+        </Section>
+
       </Box>
     </Box>
-
-    <br />
-
-    <Section delay={0.1}>
-      <Paragraph>
-        some text
-      </Paragraph>
-      <Heading as="h2" variant="section-title">
-        some tect
-      </Heading>
-      <Paragraph>
-        some text
-      </Paragraph>
-    </Section>
-
-    <Section delay={0.3}>
-      <Heading as="h2" variant="section-title">
-        some text here
-      </Heading>
-      <Paragraph>
-        some text here
-      </Paragraph>
-    </Section>
-    <br />
-
-    <Section delay={0.3}>
-      <Heading as="p" variant="section-title">
-        some text here
-      </Heading>
-      <Paragraph>
-        some text here
-      </Paragraph>
-    </Section>
-    <br />
-
-    <Section delay={0.3}>
-      <Heading as="h2" variant="section-title">
-        some heading here too
-      </Heading>
-      <Paragraph>
-        some text here
-      </Paragraph>
-    </Section>
   </Layout >
 );
 
