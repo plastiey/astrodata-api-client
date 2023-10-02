a62d81ead1a8f7a0996fe9202abb8b94f17db8ed
kenjitheman <kenjitheman@proton.me>
2023-10-02 21:32:55 +0300

    feat: chart demo

--- components/chart.js
+++ components/chart.js
@@ -1,20 +1,59 @@
-import React, { useState, useEffect } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
 
-function Chart() {
-    const [generatedHTML, setGeneratedHTML] = useState("");
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/charts/bar")
-            .then((response) => response.text())
-            .then((data) => setGeneratedHTML(data))
-            .catch((error) => console.error("Error fetching HTML:", error));
-    }, []);
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+    },
+    title: {
+      display: true,
+      text: 'Hourly Equatorial Dst Values (REAL-TIME)',
+    },
+  },
+};
 
-    return (
-        <div>
-            <div dangerouslySetInnerHTML={{ __html: generatedHTML }}></div>
-        </div>
-    );
+const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'womans',
+      data: labels.map(() => faker.number.int({ min: -200, max: 0 })),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'mans',
+      data: labels.map(() => faker.number.int({ min: -243, max: 0 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export function Chart() {
+  return <Line options={options} data={data} />;
 }
-
-export default Chart;
