7bb4d6098b1ac42710009910a814ce2b504e1b36
kenjitheman <kenjitheman@proton.me>
2023-10-04 23:41:46 +0300

    chore: github logo++

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link, Button } from "@chakra-ui/react";
+import { IoLogoGithub } from "react-icons/io5";
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm">
-      <a href="https://github.com/kenjitheman/projx.c">
-        <p>
-          source code
-        </p>
-      </a>
-      <br />
-      &copy; {new Date().getFullYear()}{" "}
-      magneconn | all rights reserved
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      fontSize="sm"
+      color="_yellow"
+      position="fixed"
+      bottom="0"
+      left="0"
+      right="0"
+      padding="1rem"
+      zIndex="999"
+    >
+      <span>&copy;Magneconn | Next Space Lab | {new Date().getFullYear()}</span>
+      <Link
+        href="https://github.com/kenjitheman/projx.c"
+        target="_blank"
+        position="absolute"
+        bottom="2.5"
+        right="4"
+      >
+        <Button
+          variant="ghost"
+          color="_yellow"
+          fontSize="2.5rem"
+          leftIcon={<IoLogoGithub />}
+        >
+        </Button>
+      </Link>
     </Box>
   );
 };
