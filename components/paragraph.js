635c3901a118583eca304d7c1bccbb5a65c0f711
kenjitheman <kenjitheman@proton.me>
2023-10-06 02:26:48 +0300

    chore: some text changed

--- components/paragraph.js
+++ components/paragraph.js
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 
 const Paragraph = styled.p`
   text-align: justify;
+  font-weight: regular;
+  font-family: "Raleway", sans-serif;
 	font-size: 1.2rem;
   hyphens: auto;
 `;
