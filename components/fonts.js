337334ba1446a666fe4562ff79832788f43b9fa2
kenjitheman <kenjitheman@proton.me>
2023-10-07 00:10:01 +0300

    style: font changed

--- components/fonts.js
+++ components/fonts.js
@@ -1,7 +1,7 @@
 const Fonts = () => (
   <style jsx global>
     {`
-    @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;700&display=swap');
   `}
   </style>
 );
